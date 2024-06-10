5d465d4dbf0abd9030055efa8dc68b9ad500837a
ABer <112191525+Illu30qpPeHiK@users.noreply.github.com>
2024-06-10 12:15:04 +0300

    Add files via upload

--- search-foto/main.py
+++ search-foto/main.py
@@ -8,35 +8,46 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from auth_data import username, password
+import logging
 
-driver = webdriver.Safari()
+
+logging.basicConfig(level=logging.INFO)
+
+username = os.getenv('INSTAGRAM_USERNAME')
+password = os.getenv('INSTAGRAM_PASSWORD')
+
+
+options = webdriver.SafariOptions()
+
+driver = webdriver.Safari(options=options)
 
 def search_user():
     return input('Введіть username котрий треба знайти: ')
 
 def login(username, password):
-    username_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'username'))
-    )
-    username_input.clear()
-    username_input.send_keys(username)
-    time.sleep(random.randrange(2, 5))
+    try:
+        username_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'username'))
+        )
+        username_input.clear()
+        username_input.send_keys(username)
+        time.sleep(random.uniform(2, 5))
 
-    password_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'password'))
-    )
-    password_input.send_keys(password)
-    password_input.clear()
-    password_input.send_keys(password)
-    password_input.send_keys(Keys.ENTER)
-    time.sleep(random.randrange(2, 5))
+        password_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'password'))
+        )
+        password_input.clear() 
+        password_input.send_keys(password)
+        password_input.send_keys(Keys.ENTER)
+        time.sleep(random.uniform(2, 5))
+    except Exception as e:
+        print(f"Error logging in: {e}")
 
 def search(username):
     driver.get(f'https://www.instagram.com/{username}/')
 
 def get_page_source():
-    time.sleep(random.randrange(2, 5)) 
+    time.sleep(random.uniform(2, 5)) 
     return driver.page_source
 
 def get_all_fotos(page_source):
@@ -45,9 +56,10 @@ def get_all_fotos(page_source):
         find_all_foto = soup.find_all('img')
         return find_all_foto
     except Exception as e:
-        print("Не вдалося знайти зображення:", e)
+        logging.error(f"Error getting all fotos: {e}")
+        print('\n')
         return None
-    
+
 def download_images(fotos, folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -59,22 +71,19 @@ def download_images(fotos, folder):
                 img_data = requests.get(img_url).content
                 with open(f"{folder}/image_{index + 1}.jpg", 'wb') as handler:
                     handler.write(img_data)
-                print(f"Image {index + 1} downloaded: {img_url}")
+                logging.info(f"Image {index + 1} downloaded: {img_url}")
                 print('\n')
             except Exception as e:
-                print(f"Could not download image {img_url}: {e}")
-    
-
-
+                logging.error(f"Could not download image {img_url}: {e}")
 
 def main():
     try:
         driver.get('https://www.instagram.com/')
         username_to_search = search_user()
-        time.sleep(random.randrange(2, 5))
+        time.sleep(random.uniform(2, 5))
 
         login(username, password)
-        time.sleep(random.randrange(2, 5))
+        time.sleep(random.uniform(2, 5))
 
         search(username_to_search)
         page_source = get_page_source()
@@ -82,10 +91,8 @@ def main():
         
         if fotos:
             download_images(fotos, folder=username_to_search)
-            
-                
         
-        time.sleep(random.randrange(2, 5))
+        time.sleep(random.uniform(2, 5))
     finally:
         driver.close()
         driver.quit()
